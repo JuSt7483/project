db47632513beb2159be2f021cb320184382b24de
Igor <igormatnak092@gmail.com>
2022-05-27 13:05:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm('Ты здесь ?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log((answer + 5));
+
+const answers = [];
+
+answers[0] = prompt('Как вашк имя?','');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
